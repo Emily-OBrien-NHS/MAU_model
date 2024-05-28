40e22d430d0959ce13335b75c5a71b93ddab3e0f
Emily-OBrien-NHS <e.obrien6@nhs.net>
2024-05-28 13:23:24 +0100

    Model first draft and code to find duration and probability variables

--- mau_model.py
+++ mau_model.py
@@ -3,25 +3,53 @@ import simpy
 import random
 import pandas as pd
 import numpy as np
-os.chdir('C:/Users/obriene/Projects/MAU_model/outputs')
+os.chdir('C:/Users/obriene/Projects/MAU model/outputs')
 
 class params():
+    scenario_name = 'baseline'
     #run times and iterations
-    warm_up = 30
-    run_time = 2400
-    iterations = 10
+    warm_up = 0
+    run_time = 525600
+    iterations = 1
+    #times of processes
+    mean_amb_arr = 18
+    mean_walk_arr = 7
+    mean_ed = 283
+    mean_mau = 1784
     #resources
     no_mau_beds = 52
-    #timings
     #split probabilities
-    hosp_adm_prob = 0.79
+    ed_disc_prob = 0.64
+    dta_admit_elsewhere_prob = 0.67
+    mau_disc_prob = 0.2
     #empty list for results
     results = []
 
 class spawn_patient:
-    def __init__(self, p_id):
-        #set up patient id
+    def __init__(self, p_id, eu_disc_prob, dta_admit_elsewhere_prob, mau_disc_prob):
+        #set up patient id and arrival type
         self.id = p_id
+        self.arrival = ''
+
+        #work out probabilities of patient following each path
+        #Does patient get discharged from ED
+        self.decide_ed_disc_prob = random.uniform(0,1)
+        self.ed_disc = True if self.decide_ed_disc_prob <= eu_disc_prob else False
+        #Does patient get admitted to MAU
+        self.decide_mau_adm_prob = random.uniform(0,1)
+        self.dta_admit_elsewhere = True if self.decide_mau_adm_prob <= dta_admit_elsewhere_prob else False
+        #Does patient get discharged from MAU
+        self.decide_mau_disc_prob = random.uniform(0,1)
+        self.mau_disc = True if self.decide_mau_disc_prob <= mau_disc_prob else False
+        
+        #Establish variables to store results
+        self.ed_arrival_time = np.nan
+        self.ed_leave_time = np.nan
+        self.enter_mau_queue = np.nan
+        self.leave_mau_queue = np.nan
+        self.leave_mau = np.nan
+        self.note = ''
+
 
 class mau_model:
     def __init__(self, run_number):
@@ -30,33 +58,91 @@ class mau_model:
         self.patient_counter = 0
         self.run_number = run_number
         #establish resources
+        self.ed = simpy.Resource(self.env, capacity=100000000) 
         self.mau_bed = simpy.Resource(self.env, capacity=params.no_mau_beds)
-
-    def generate_mau_arrivals(self):
+        
+    def generate_walkin_ed_arrivals(self):
         while True:
             #up patient counter and spawn a new patient
             self.patient_counter += 1
-            p = spawn_patient(self.patient_counter)
+            p = spawn_patient(self.patient_counter, params.ed_disc_prob,
+                              params.dta_admit_elsewhere_prob, params.mau_disc_prob)
+            p.arrival = 'Walk-in'
             #begin patient ed process
             self.env.process(self.mau_journey(p))
             #randomly sample the time until the next patient arrival
-            sampled_interarrival = random.expovariate(1.0 / params.mean_inter_arr)
+            sampled_interarrival = random.expovariate(1.0 / params.mean_walk_arr)
+            yield self.env.timeout(sampled_interarrival)
+    
+    def generate_ambulance_ed_arrivals(self):
+        while True:
+            #up patient counter and spawn a new patient
+            self.patient_counter += 1
+            p = spawn_patient(self.patient_counter, params.ed_disc_prob,
+                              params.dta_admit_elsewhere_prob, params.mau_disc_prob)
+            p.arrival = 'Ambulance'
+            #begin patient ed process
+            self.env.process(self.mau_journey(p))
+            #randomly sample the time until the next patient arrival
+            sampled_interarrival = random.expovariate(1.0 / params.mean_amb_arr)
             yield self.env.timeout(sampled_interarrival)
         
     def mau_journey(self, patient):
-        patient.start_time = self.env.now
+
+        patient.ed_arrival_time = self.env.now 
+
+        #ED time
+        with self.ed.request() as req:
+            yield req
+            #randomly sample the time spent in ED
+            sampled_ed_time = random.expovariate(1.0 / params.mean_ed)
+            yield self.env.timeout(sampled_ed_time)
+        
+        patient.ed_leave_time = self.env.now
+
+        if patient.ed_disc:
+            #patient discharged from ED and leaves the process
+            patient.note = 'Discharged from ED'
+        else:
+            #Patient to be admitted somewhere else
+            if patient.dta_admit_elsewhere:
+                #Patient admitted elsewhere from dta and leaves the process
+                patient.note = 'Admitted elsewhere'
+            else:
+                #Patient begins wait for mau bed
+                patient.enter_mau_queue = self.env.now
+
+                with self.mau_bed.request() as req:
+                    yield req
+                    #record how long the patient was in the MAU queue
+                    patient.leave_mau_queue = self.env.now
+                    #randomly sample the time spent in an MAU bed
+                    sampled_mau_duration = random.expovariate(1.0 / params.mean_mau)
+                    yield self.env.timeout(sampled_mau_duration)
+
+                patient.leave_mau = self.env.now
+
+                if patient.mau_disc:
+                    #Patient discharged from MAU
+                    patient.note = 'Discharged from MAU'
+                else:
+                    #Patient admitted to specialty ward
+                    patient.note = 'Admitted to Specialty Ward'
 
         #If past the warm up period, start recoring results
-        if self.env.now > params.warm_up:
-            self.store_patient_results(patient)
+        #if self.env.now > params.warm_up:
+        self.store_patient_results(patient)
 
     #Save patients results in a list to turn into a dataframe later
     def store_patient_results(self, patient):
-        params.results.append([])
+        params.results.append([self.run_number, patient.id, patient.arrival, patient.ed_arrival_time,
+                               patient.ed_leave_time, patient.enter_mau_queue, patient.leave_mau_queue,
+                               patient.leave_mau, patient.note])
 
     def run(self):
         #Run process for the run time specified
-        self.env.process(self.generate_ed_arrivals())
+        self.env.process(self.generate_walkin_ed_arrivals())
+        self.env.process(self.generate_ambulance_ed_arrivals())
         self.env.run(until=(params.warm_up + params.run_time))
 
 class run_the_model:
@@ -68,5 +154,10 @@ class run_the_model:
 
     #put full results into a dataframe and export to csv
     df = pd.DataFrame(params.results,
-                      columns= [])
-    df.to_csv('mau results.csv', index=False)
+                      columns= ['run', 'patient id', 'ed arrival type', 'ed arrival time',
+                                'ed leave time', 'enter mau queue', 'leave mau queue',
+                                'leave mau', 'note']).sort_values(by='patient id')
+    df.to_csv(params.scenario_name + ' mau results.csv', index=False)
+
+#df['ED time'] = df['ed leave time'] - df['ed arrival time']
+#df['MAU Queue'] = df['leave mau queue'] - df['enter mau queue']
---
+++ mau_parameters.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+#############################################################################
+#Create the engine
+SDMart_engine = create_engine('mssql+pyodbc://@SDMartDataLive2/PiMSMarts?'\
+                           'trusted_connection=yes&driver=ODBC+Driver+17'\
+                               '+for+SQL+Server')
+cl3_engine = create_engine('mssql+pyodbc://@cl3-data/DataWarehouse?'\
+                           'trusted_connection=yes&driver=ODBC+Driver+17'\
+                               '+for+SQL+Server')
+
+#sql query
+mau_query = """SET NOCOUNT ON
+select prvsp_refno, MAUStart = min(sstay_start_dttm), MAUEnd = max(sstay_end_dttm)
+  into #MAUStay
+  from [PiMSMarts].[dbo].[ip_movements]
+  where move_reason_sp = 's'
+  and sstay_ward_code in ('RK950MAU','RK950AMW')
+  and sstay_end_dttm between '01-Apr-2023 00:00:00' and '31-Mar-2024 00:00:00'
+  group by prvsp_refno
+
+  select mstay.*, post_mau.sstay_ward_code, post_mau.sstay_start_dttm,
+  case when (post_mau.sstay_ward_code is NULL or post_mau.sstay_ward_code = 'RK950101' or post_mau.sstay_ward_code = 'RK950113') then 'MAU Discharge' --If they go to DL
+		when post_mau.sstay_ward_code like 'RK950%' then 'Transfer to Acute Ward'
+		when (post_mau.sstay_ward_code like 'RK901%' or
+			post_mau.sstay_ward_code like 'RK906%' or
+			post_mau.sstay_ward_code like 'RK935%' or
+			post_mau.sstay_ward_code like 'RK959%' or 
+			post_mau.sstay_ward_code like 'RK922%') then 'Transfer to Community Ward'
+			else post_mau.sstay_ward_code end as DischRoute
+	,totalLoS.DischargeDttm			
+  from #MAUStay mstay
+  left join [PiMSMarts].[dbo].[ip_movements] post_mau
+	on post_mau.prvsp_refno = mstay.prvsp_refno
+	and post_mau.move_reason_sp = 's'
+	and post_mau.sstay_start_dttm = mstay.MAUEnd ---get next ward after MAU
+  left join (select prvsp_refno,  DischargeDttm = max(sstay_end_dttm)
+			from [PiMSMarts].[dbo].[ip_movements] 
+			where sstay_ward_code not in ('RK950MAU','RK950AMW')
+			and sstay_ward_code not like 'rk901%'
+			and sstay_ward_code not like 'rk959%'
+			and sstay_ward_code not like 'rk935%'
+			and sstay_ward_code not like 'rk906%'
+			and move_reason_sp = 'S'
+			and sstay_end_dttm between '01-Jan-2023 00:00:00' and getdate()
+			group by prvsp_refno
+			)totalLos
+	on totalLos.prvsp_refno = mstay.prvsp_refno
+	and totalLos.DischargeDttm > mstay.MAUEnd ---only ward moves that happen after MAU
+
+	order by mstay.prvsp_refno
+"""
+
+ed_query = """
+SELECT [ArrivalDateTime]
+      ,[ArrivalModeDescription]
+	  ,[DecidedToAdmitDateTime]
+	  ,[BedRequestedDateTime]
+      ,[DischargeDateTime]
+      ,[DischargeDestinationGroup]
+      ,[ActualDischargeDestinationDescription]
+      ,[LengthOfStay]
+	  ,AdmitPrvspRefno
+  FROM [DataWarehouse].[ED].[vw_EDAttendance]
+  WHERE DischargeDateTime between '01-Apr-2023 00:00:00' and '31-Mar-2024 00:00:00'
+  AND IsNewAttendance = 'Y'
+"""
+
+mau_df = pd.read_sql(mau_query, SDMart_engine).rename(columns={'prvsp_refno':'AdmitPrvspRefno'})
+ed_df = pd.read_sql(ed_query, cl3_engine)
+
+#Close the connection
+SDMart_engine.dispose()
+cl3_engine.dispose()
+
+#####################################################################################
+#interarrival times for ambulance and walkin
+#ambulance
+amb_df = ed_df.loc[ed_df['ArrivalModeDescription'].str.contains('ambulance'),
+                   ['ArrivalDateTime', 'ArrivalModeDescription']].sort_values(by='ArrivalDateTime')
+amb_df['DateShifted'] = amb_df['ArrivalDateTime'].shift(-1)
+amb_df['TimeBetweenArrivals'] = (amb_df['DateShifted'] - amb_df['ArrivalDateTime']) / pd.Timedelta(minutes=1)
+print(f'Average time between ambulance arrivals is {amb_df['TimeBetweenArrivals'].mean():.0f} minutes')
+
+#walk in
+wlkin_df = ed_df.loc[~ed_df['ArrivalModeDescription'].str.contains('ambulance'),
+                   ['ArrivalDateTime', 'ArrivalModeDescription']].sort_values(by='ArrivalDateTime')
+wlkin_df['DateShifted'] = wlkin_df['ArrivalDateTime'].shift(-1)
+wlkin_df['TimeBetweenArrivals'] = (wlkin_df['DateShifted'] - wlkin_df['ArrivalDateTime']) / pd.Timedelta(minutes=1)
+print(f'Average time between walk in arrivals is {wlkin_df['TimeBetweenArrivals'].mean():.0f} minutes')
+
+#Average time in ED until DTA
+ed_df['TimeToDTA'] = (ed_df['DecidedToAdmitDateTime'] - ed_df['ArrivalDateTime']) / pd.Timedelta(minutes=1)
+print(f'Average time in ED until DTA is {ed_df['TimeToDTA'].mean():.0f} minutes')
+
+#average time spent in MAU
+mau_df['MAU Time'] = (mau_df['MAUEnd'] - mau_df['MAUStart']) / pd.Timedelta(minutes=1)
+print(f'Average time spent in MAU is {round(mau_df['MAU Time'].mean())} minutes')
+
+#Proportion of ED patients who get discharged
+prop = ed_df['AdmitPrvspRefno'].notnull().value_counts(normalize=True)
+print(f'Proportion of patients being discharged from ED is {prop[False]:.2f}')
+
+#Proportion of admitted that are admitted to MAU
+merged = ed_df.loc[~ed_df['AdmitPrvspRefno'].isna()].merge(mau_df, on='AdmitPrvspRefno', how='left')
+prop = merged['sstay_start_dttm'].notnull().value_counts(normalize=True)
+print(f'Proportion of patients that are admitted to somewhere other than MAU is {prop[False]:.2f}')
+
+#proportion that get discharged home from MAU
+mau_disc = (mau_df.loc[mau_df['DischRoute'] == 'MAU Discharge', 'DischRoute'].count()
+            / mau_df['DischRoute'].count())
+print(f'Proportion of patients that get discharged from MAU is {mau_disc:.2f}')
